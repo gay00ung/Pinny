3e01ccf30bfd7ccab8142b1d0b8fbf72264b22ee
gayoung <gayoung990911@gmail.com>
2025-09-29 12:12:01 +0900

    Initial project setup for Kotlin Multiplatform app

---
+++ composeApp/src/commonTest/kotlin/net/ifmain/pinny/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package net.ifmain.pinny
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/net/ifmain/pinny/MainViewController.kt
@@ -0,0 +1,5 @@
+package net.ifmain.pinny
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
